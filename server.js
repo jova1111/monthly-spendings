ded341fd4b2b6eebb9e7ea6ee5c27447a90dccc1
Wuksha <wuksha95@hotmail.com>
2018-07-29 03:05:30 +0200

    server.js typo fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(exoress.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(request, response) {
     response.sendfile(__dirname + "/dist/index.html");
 });
