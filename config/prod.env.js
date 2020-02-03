b462848a0a3e0d0f7618c085cea9bc65e023440d
Jova <jocad95@hotmail.com>
2020-02-03 20:14:41 +0100

    Changed prod env endpoint.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  LINK: '"https://wukshas-monthly-spendings-back.herokuapp.com/api"'
+  LINK: '"https://monthly-spendings-api.herokuapp.com/api"'
 }
